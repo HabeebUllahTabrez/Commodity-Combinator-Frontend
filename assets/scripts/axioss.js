26abf646bfbc4366d3782e9024d735f6964f015b
HabeebUllahTabrez <ullahabeeb11@gmail.com>
2022-12-25 07:43:44 +0530

    final changes

--- assets/scripts/axioss.js
+++ assets/scripts/axioss.js
@@ -8,15 +8,10 @@ const fetchButton = document.querySelector("#available-posts button");
 // Just include the script and call axios globally with a method!
 
 // All functions remain the same
+const post_1 = document.getElementsByClassName("post_1");
+const post_2 = document.getElementsByClassName("post_2");
+const post_3 = document.getElementsByClassName("post_3");
 
-function generatePostTemplate(post) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body.toUpperCase();
-    postEl.querySelector("li").id = post.id;
-
-    return postEl;
-}
 
 async function fetchPosts() {
     try {
@@ -24,10 +19,46 @@ async function fetchPosts() {
         console.log(response);
         const responsePosts = response.data;
 
-        for (i = 0; i < 10; i++) {
-            const postEl = generatePostTemplate(responsePosts[i]);
-            listElement.append(postEl);
+        // for (i = 0; i < 10; i++) {
+        //     const postEl = generatePostTemplate(responsePosts[i]);
+        //     listElement.append(postEl);
+        // }
+        if (responsePosts.length >= 1) {
+        document.querySelector(".button_1").textContent = "No of commodities data available = " + responsePosts.length || " ";
+        document.querySelector(".marketName1").textContent ="MarketName: " + responsePosts[0].marketName;
+        document.querySelector(".cmdtyID1").textContent = "cmdtyID: " + responsePosts[0].cmdtyID;
+        document.querySelector(".cmdtyName1").textContent = "cmdtyName: " + responsePosts[0].cmdtyName;
+        document.querySelector(".price1").textContent = "price: " + responsePosts[0].price;
         }
+////////////////////////////////////////////////////////////
+if (responsePosts.length >= 2) {
+        document.querySelector(".marketName2").textContent ="MarketName: " + responsePosts[1].marketName;
+        document.querySelector(".cmdtyID2").textContent = "cmdtyID: " + responsePosts[1].cmdtyID;
+        document.querySelector(".cmdtyName2").textContent = "cmdtyName: " + responsePosts[1].cmdtyName;
+        document.querySelector(".price2").textContent = "price: " + responsePosts[1].price;
+}
+////////////////////////////////////////////////////////////////////////
+if (responsePosts.length >= 3) {
+        document.querySelector(".marketName3").textContent ="MarketName: " + responsePosts[2].marketName;
+        document.querySelector(".cmdtyID3").textContent = "cmdtyID: " + responsePosts[2].cmdtyID;
+        document.querySelector(".cmdtyName3").textContent = "cmdtyName: " + responsePosts[2].cmdtyName;
+        document.querySelector(".price3").textContent = "price: " + responsePosts[2].price;
+}
+        ////////////////////////////////////////////////////////////////////////
+        if (responsePosts.length >= 4) {
+        document.querySelector(".marketName4").textContent ="MarketName: " + responsePosts[3].marketName;
+        document.querySelector(".cmdtyID4").textContent = "cmdtyID: " + responsePosts[3].cmdtyID;
+        document.querySelector(".cmdtyName4").textContent = "cmdtyName: " + responsePosts[3].cmdtyName;
+        document.querySelector(".price4").textContent = "price: " + responsePosts[3].price;
+        }
+         ////////////////////////////////////////////////////////////////////////
+         if (responsePosts.length >= 5) {
+         document.querySelector(".marketName5").textContent ="MarketName: " + responsePosts[4].marketName || " ";
+         document.querySelector(".cmdtyID5").textContent = "cmdtyID: " + responsePosts[4].cmdtyID || " ";
+         document.querySelector(".cmdtyName5").textContent = "cmdtyName: " + responsePosts[4].cmdtyName || " ";
+         document.querySelector(".price5").textContent = "price: " + responsePosts[4].price || " ";
+         }
+        
     } catch (error) {
         alert(error.message);
         console.log(error.response);
